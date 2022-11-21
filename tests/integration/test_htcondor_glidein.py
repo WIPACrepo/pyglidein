69e4700a9eabade80fb53d4ce633754248b8141e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 15:23:47 +0000

    Adding tarfile member sanitization to extractall()

--- tests/integration/test_htcondor_glidein.py
+++ tests/integration/test_htcondor_glidein.py
@@ -127,7 +127,26 @@ class TestHTCondorGlidein(unittest.TestCase):
         logfile_opener = urllib.URLopener()
         logfile_opener.retrieve(url, log_filename)
         with tarfile.open(log_filename, 'r:gz') as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
         logdir = glob.glob('log.*')[0]
         self.assertTrue(os.path.exists(os.path.join(logdir, 'MasterLog')),
                         msg='Failed to download logfile: {}'.format(url))
