351551adb7af03e727957df3d0613bf529f420f9
Benedikt Riedel <benedikt.riedel@gmail.com>
2026-07-08 13:20:42 -0500

    adding stampede3 helper script

---
+++ configs/helpers/submit_stampede3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export WORK2=/work2/04799/tg840985/stampede3
+
+counter=$(squeue -u $USER -t PENDING -p h100 | wc -l)
+
+while [ $counter -le 4 ];
+do
+    sbatch /home1/04799/tg840985/pyglidein2/configs/stampede3_gpu.slurm
+    counter=$(squeue -u $USER -t PENDING -p h100 | wc -l)
+done
+
