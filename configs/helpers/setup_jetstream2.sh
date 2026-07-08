260fd0d83559a77ca1b9f83c4cd822522e60bd78
rocky Cloud User <rocky@gpu-testing.novalocal>
2026-07-08 17:29:28 +0000

    Adding JetStream2 Scripts

---
+++ configs/helpers/setup_jetstream2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This assume we are on a RHEL style system
+
+
+cd $HOME
+
+# Install apptainer
+sudo dnf install -y epel-release
+sudo dnf install -y apptainer
+
+# Get required image
+apptainer pull osgvo-docker-pilot_25_cuda_11.8 docker://hub.opensciencegrid.org/osg-htc/ospool-ep:25-cuda_11_8_0-release
+
+# Settung up cvmfsexec
+git clone https://github.com/cvmfs/cvmfsexec.git
+
+$HOME/cvmfsexec/makedist osg
+
---
+++ configs/run_jetstream2_gpu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+unset LD_PRELOAD
+
+
+export GLIDEIN_Site="IU Jetstream 2"
+export SINGULARITY_BIN=apptainer
+export TOKEN=eyJhbGciOiJIUzI1NiIsImtpZCI6IlBPT0wifQ.eyJpYXQiOjE3NjIzNzkzNjMsImlzcyI6ImdsaWRlaW4tY20uaWNlY3ViZS53aXNjLmVkdSIsImp0aSI6IjBiM2IzOGI5MzlkMGY0ZDM0ZDIwMDdjNWNlNjc1Mjk3Iiwic2NvcGUiOiJjb25kb3I6XC9SRUFEIGNvbmRvcjpcL1dSSVRFIGNvbmRvcjpcL0RBRU1PTiBjb25kb3I6XC9BRFZFUlRJU0VfU1RBUlREIGNvbmRvcjpcL0FEVkVSVElTRV9NQVNURVIiLCJzdWIiOiJweWdsaWRlaW5AaWNlY3ViZS53aXNjLmVkdSJ9.lM0ED5dEEWCEg1s5d4toNHk9vkbLapwgm6yx--OBcAE
+export BASE_IMAGE=/home/rocky/osgvo-docker-pilot_25_cuda_11.8
+
+export CUDA_VISIBLE_DEVICES=0
+export CPUS=16
+export MEMORY=60000
+export GPUS=CUDA0
+# export USE_CVMFSEXEC=True
+
+counter=1
+
+while [ $counter -le 5 ]
+do 
+    export SCRATCHDIR=$(mktemp -d)
+    cd $SCRATCHDIR
+    $HOME/cvmfsexec/cvmfsexec config-osg.opensciencegrid.org oasis.opensciencegrid.org singularity.opensciencegrid.org icecube.opensciencegrid.org -- ~/pyglidein2/pyglidein/glidein_start.sh 1> $HOME/glidein.out 2> $HOME/glidein.err
+done
+# while true; do 
+#     ~/pyglidein2/pyglidein/glidein_start.sh
+# done
