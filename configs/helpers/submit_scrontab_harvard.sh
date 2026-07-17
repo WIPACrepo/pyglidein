7af078d05797a8a1ee3e600527cb3fd3b28d9487
Benedikt Riedel <benedikt.riedel@gmail.com>
2026-07-17 12:28:37 -0400

    editing the scron job

--- configs/helpers/submit_scrontab_harvard.sh
+++ configs/helpers/submit_scrontab_harvard.sh
@@ -3,4 +3,14 @@
 find /n/netscratch/arguelles_delgado_lab/Lab/IceCube/prod/ -maxdepth 1 -type d -ctime +2 | xargs -P 8 rm -rf &
 find /n/home00/briedel/out/  -type f -ctime +2 | xargs -P 8 rm -rf &
 
-sbatch /n/home00/briedel/pyglidein2/configs/harvard.slurm
+# Have at least 2 jobs arrays in the queue at all times
+
+if [ $(squeue --me -t PENDING | grep "gpu_reque" | wc -l) -le 2 ]; then
+    echo "SUBMIT GPU"
+    sbatch /n/home00/briedel/pyglidein2/configs/harvard_gpu.slurm
+fi
+
+if [ $(squeue --me -t PENDING | grep "serial_re" | wc -l) -le 2 ]; then
+    echo "SUBMIT CPU"
+    sbatch /n/home00/briedel/pyglidein2/configs/harvard_cpu.slurm
+fi
