0f1b8e33e14d286aca3b778b3b1745091d2183d1
Benedikt Riedel <benedikt.riedel@gmail.com>
2026-07-17 11:45:43 -0500

    admins complained so limiting submit further

--- configs/helpers/submit_stampede3.sh
+++ configs/helpers/submit_stampede3.sh
@@ -2,11 +2,25 @@
 
 export WORK2=/work2/04799/tg840985/stampede3
 
-counter=$(squeue -u $USER -p h100 | wc -l)
+# at most we are allowed to submit 4 jobs into the h100 queue at a given time
+max_submit=4
 
-while [ $counter -le 4 ];
-do
+if [ $(squeue -u $USER -p h100 | grep "h100" | wc -l) -ge $max_submit ]; then
+    exit
+fi
+
+for i in $(seq "1" "$max_submit"); do
+    if [ $(squeue -u $USER -p h100 | wc -l) -ge $max_submit ]; then
+        break
+    fi
     sbatch /home1/04799/tg840985/pyglidein2/configs/stampede3_gpu.slurm
-    counter=$(squeue -u $USER -p h100 | wc -l)
 done
+
+# counter=$(squeue -u $USER -p h100 | wc -l)
+
+# while [ $counter -le 4 ];
+# do
+#     sbatch /home1/04799/tg840985/pyglidein2/configs/stampede3_gpu.slurm
+#     counter=$(squeue -u $USER -p h100 | wc -l)
+# done
 
