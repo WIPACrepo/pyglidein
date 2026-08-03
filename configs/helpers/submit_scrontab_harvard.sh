4bda5dfd8c602f3dd6a2cae15404c378739830c4
Evelyn-H <ehobert@icecube.wisc.edu>
2026-08-03 15:49:11 -0500

    change harvard username / home folder to take over from briedel

--- configs/helpers/submit_scrontab_harvard.sh
+++ configs/helpers/submit_scrontab_harvard.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 find /n/netscratch/arguelles_delgado_lab/Lab/IceCube/prod/ -maxdepth 1 -type d -ctime +2 | xargs -P 8 rm -rf &
-find /n/home00/briedel/out/  -type f -ctime +2 | xargs -P 8 rm -rf &
+find /n/home13/ehobert/out/  -type f -ctime +2 | xargs -P 8 rm -rf &
 
 # Have at least 2 jobs arrays in the queue at all times
 
 if [ $(squeue --me -t PENDING | grep "gpu_reque" | wc -l) -le 2 ]; then
     echo "SUBMIT GPU"
-    sbatch /n/home00/briedel/pyglidein2/configs/harvard_gpu.slurm
+    sbatch /n/home13/ehobert/pyglidein2/configs/harvard_gpu.slurm
 fi
 
 if [ $(squeue --me -t PENDING | grep "serial_re" | wc -l) -le 2 ]; then
     echo "SUBMIT CPU"
-    sbatch /n/home00/briedel/pyglidein2/configs/harvard_cpu.slurm
+    sbatch /n/home13/ehobert/pyglidein2/configs/harvard_cpu.slurm
 fi
