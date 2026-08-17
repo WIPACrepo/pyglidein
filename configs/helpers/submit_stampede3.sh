190166e160ea4646744acbccdcf09945ad94e783
Evelyn-H <ehobert@icecube.wisc.edu>
2026-08-17 12:53:32 -0500

    crontab

--- configs/helpers/submit_stampede3.sh
+++ configs/helpers/submit_stampede3.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-export WORK2=/work2/04799/tg840985/stampede3
+# intended crontab entry:
+# (note: stampede3 has regular crontab, not scrontab. and it's per login node, so always gotta log in to the same one)
+# 0 */4 * * * $HOME/glideins/pyglidein2/configs/helpers/submit_stampede3.sh >> /scratch/11739/ehobert/logs/cron.log 2>&1
 
-# at most we are allowed to submit 4 jobs into the h100 queue at a given time
-max_submit=4
+# h100 allows up to 4 submitted jobs despite running only 2 at a time
+max_jobs=4
 
-if [ $(squeue -u $USER -p h100 | grep "h100" | wc -l) -ge $max_submit ]; then
+count=$(squeue -u "$USER" -p h100 -h | grep "glidein" | wc -l)
+
+if [ "$count" -lt "$max_jobs" ]; then
+    echo "SUBMIT H100 ($count of $max_jobs in queue)"
+    sbatch "$HOME/glideins/pyglidein2/configs/stampede3_gpu.slurm"
+else
     exit
 fi
-
-for i in $(seq "1" "$max_submit"); do
-    if [ $(squeue -u $USER -p h100 | wc -l) -ge $max_submit ]; then
-        break
-    fi
-    sbatch /home1/04799/tg840985/pyglidein2/configs/stampede3_gpu.slurm
-done
-
-# counter=$(squeue -u $USER -p h100 | wc -l)
-
-# while [ $counter -le 4 ];
-# do
-#     sbatch /home1/04799/tg840985/pyglidein2/configs/stampede3_gpu.slurm
-#     counter=$(squeue -u $USER -p h100 | wc -l)
-# done
-
