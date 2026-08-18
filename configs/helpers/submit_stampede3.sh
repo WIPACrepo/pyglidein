2669c0702309379d6710e94ad03754b873447fcb
Evelyn Hobert <ehobert@icecube.wisc.edu>
2026-08-18 15:57:27 -0500

    move cpu job pilots to scratch

--- configs/helpers/submit_stampede3.sh
+++ configs/helpers/submit_stampede3.sh
@@ -4,6 +4,13 @@
 # (note: stampede3 has regular crontab, not scrontab. and it's per login node, so always gotta log in to the same one)
 # 0 */4 * * * $HOME/glideins/pyglidein2/configs/helpers/submit_stampede3.sh >> /scratch/11739/ehobert/logs/cron.log 2>&1
 
+# no accidental disk wipes
+set -u
+
+# clean up stale pilot scratch dirs; jobs max 48h so +3d is safe
+find /scratch/11739/ehobert/glideins -mindepth 2 -maxdepth 2 -type d -mtime +3 -print0 \
+    | xargs -0 -r -P 8 rm -rf
+
 # h100 allows up to 4 submitted jobs despite running only 2 at a time
 max_h100_jobs=4
 h100_count=$(squeue -u "$USER" -p h100 -h | grep "glidein" | wc -l)
